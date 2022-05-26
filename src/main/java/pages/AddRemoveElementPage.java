3b334b1b0a355068ac5c0b952b9491f4d9bf804f
Dominik Bogdanowicz <dominik.bogdanowicz.kontakt@gmail.com>
2022-05-26 13:09:45 +0200

    Create AddRemoveElementPage class

---
+++ src/main/java/pages/AddRemoveElementPage.java
@@ -0,0 +1,27 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class AddRemoveElementPage {
+
+    private WebDriver driver;
+    private By addButton = By.cssSelector("div.example>button");
+    private By deleteButton = By.cssSelector("div#elements>button");
+
+    public AddRemoveElementPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public void clickAddButton(){
+        driver.findElement(addButton).click();
+    }
+
+    public void clickDeleteButton(){
+        driver.findElement(deleteButton).click();
+    }
+
+    public int getDeleteButtonsCount(){
+        return driver.findElements(deleteButton).size();
+    }
+}
