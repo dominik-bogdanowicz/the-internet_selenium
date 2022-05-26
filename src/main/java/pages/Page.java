97eecc32598b1b5bd4267827dd5b8813cb8a20a1
Dominik Bogdanowicz <dominik.bogdanowicz.kontakt@gmail.com>
2022-05-26 12:15:15 +0200

    Create folder structure for Page Object Model and Page superclass

---
+++ src/main/java/pages/Page.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class Page {
+    private WebDriver driver;
+
+    public Page(WebDriver driver){
+        this.driver = driver;
+    }
+}
