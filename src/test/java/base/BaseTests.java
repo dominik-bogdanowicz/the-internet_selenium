27a8c49029535d71e8dda6cfaeb4cedbb08062d0
Dominik Bogdanowicz <dominik.bogdanowicz.kontakt@gmail.com>
2022-09-20 23:09:55 +0200

    Configure RemoteWebDriver

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -2,10 +2,16 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class BaseTests {
     private WebDriver driver;
@@ -13,10 +19,10 @@ public class BaseTests {
     private String homePageUrl = "https://the-internet.herokuapp.com/";
 
     @BeforeClass
-    public void setUp(){
-        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-
-        driver = new ChromeDriver();
+    public void setUp() throws MalformedURLException {
+        //System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        driver = new RemoteWebDriver(new URL("http://172.17.0.3:4444"), chromeOptions);
         homePage = new HomePage(driver);
 
         goHome();
